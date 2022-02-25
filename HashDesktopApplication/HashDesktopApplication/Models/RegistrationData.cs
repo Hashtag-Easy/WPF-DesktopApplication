59b058c0a42f813244315117526c135e4dc8a75a
Kacper Żabiński <kacperzab0@wp.pl>
2022-02-25 21:11:38 +0100

    [0.1.0] Init Commit

---
+++ HashDesktopApplication/HashDesktopApplication/Models/RegistrationData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashDesktopApplication.Models
+{
+    public class RegistrationData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+
+        public RegistrationData()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            UserName = string.Empty;
+        }
+    }
+}
