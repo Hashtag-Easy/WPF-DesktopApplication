81ba0a69f778284a37d92df0793fcda0ea4e6cbd
Kacper Żabiński <kacperzab0@wp.pl>
2022-03-19 19:53:09 +0100

    [0.1.2] Added first models.

--- HashDesktopApplication/HashDesktopApplication/obj/Debug/net6.0-windows/App.g.i.cs
+++ HashDesktopApplication/HashDesktopApplication/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace HashDesktopApplication {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace HashDesktopApplication {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             HashDesktopApplication.App app = new HashDesktopApplication.App();
             app.InitializeComponent();
